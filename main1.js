1ea167ad2a0b0edd95ae37f70fe879615c61f6a0
howelljian <114792721+howelljian@users.noreply.github.com>
2024-03-19 17:50:29 -0700

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,17 @@
+
+function easy()
+{
+  window.location = "index2.html";
+}
+
+
+function standard()
+{
+  window.location = "index.html";
+}
+
+function hard()
+{
+  window.location = "index3.html";
+
+}
